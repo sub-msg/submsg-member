d01e81f877b1ac2dd90865ef8d5379f532e33635
dogdog7788 <dogdog7788@qq.com>
2016-05-20 20:35:28 +0800

    工程提交

---
+++ src/main/java/cn/submsg/member/interceptor/NoLoginInterceptor.java
@@ -0,0 +1,30 @@
+package cn.submsg.member.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.sr178.module.web.action.BaseActionSupport;
+
+public class NoLoginInterceptor extends AbstractInterceptor{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		
+//		BaseActionSupport aldAction = null;
+		Object obj = invocation.getAction();
+		
+		if (obj instanceof BaseActionSupport) {
+//			aldAction = (BaseActionSupport) invocation.getAction();
+			return invocation.invoke();
+		}else{
+			//不支持这种方式 直接中断
+			String className = obj.getClass().getCanonicalName();
+			throw new RuntimeException("ACTION继承的类非BaseActionSupport"+className);
+		}
+	}
+
+}
---
+++ src/main/webapp/globle_error.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ include file="/common/taglib.jsp"%>
+<script type="text/javascript">
+		alert("服务器开小差，请稍后访问!");
+		history.go(-1);
+</script>
